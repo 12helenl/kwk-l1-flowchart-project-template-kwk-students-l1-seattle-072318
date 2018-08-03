8336f6a86703015dd671d002e1407a3833724ee7
12helenl <12helenl@gmail.com>
2018-08-03 17:56:50 +0000

    my message!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
-class ApplicationController < Sinatra::Base
 
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+  get '/' do
+    return erb :index
+  end
+  
+  post '/' do
+    @the_user = params[:user]
+    return html :three_sections
+  end
+
+
+
 end
+
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
-class Question
+# class Question
 
-end
+
+# end
+
